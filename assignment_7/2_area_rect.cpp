780b87a4a4b6f46039433ba65ec786b1fa651a95
POWER VHD <kshitijprasad6@gmail.com>
2026-01-18 23:56:29 +0530

    Assign Updated

---
+++ assignment_7/2_area_rect.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    long long l;
+    long long b;
+
+    cout << "Enter the length: "; 
+    cin >> l;
+    
+    cout << "Enter the breadth: ";
+    cin >> b;
+
+    long long area = l*b;
+
+    cout << "Area of Rectangle: " << area;
+
+}
